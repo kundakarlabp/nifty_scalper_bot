35708deeaa1cf8baa9908429ee1131b62a16114a
Kundakarlabp <kundakarlabp@gmail.com>
2026-04-09 22:07:43 +0530

    Update app.py

--- src/nifty_scalper_bot/core/app.py
+++ src/nifty_scalper_bot/core/app.py
@@ -37,7 +37,7 @@ from typing import (
 )
 
 import pytz
-from journal.trade_journal import TradeJournal
+from nifty_scalper_bot.journal.trade_journal import TradeJournal
 
 from nifty_scalper_bot.config.paths import get_data_dir
 from nifty_scalper_bot.data.instruments import ensure_sqlite, load_rows_for_resolver
