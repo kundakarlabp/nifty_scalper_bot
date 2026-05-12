a57bcbf78e06c424e5a082d8cd11191b56888936
Kundakarlabp <kundakarlabp@gmail.com>
2026-05-12 11:14:51 +0530

    fix: remove unintended legacy import shim

--- market_data_manager.py
+++
@@ -1,3 +0,0 @@
-"""Compatibility shim for legacy tests importing market_data_manager. Args: none. Returns: module exports. Raises: none."""
-
-from nifty_scalper_bot.data.market_data_manager import *  # noqa: F401,F403
