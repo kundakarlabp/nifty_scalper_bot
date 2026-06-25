f4e8df8617c94dd48af299915f42bef2fd3cc150
Kundakarlabp <kundakarlabp@gmail.com>
2026-06-25 07:56:47 +0530

    Update adaptive facade tests

--- tests/execution/test_adaptive_trailing_facade.py
+++ tests/execution/test_adaptive_trailing_facade.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import importlib
 
 import nifty_scalper_bot.execution as execution
-from nifty_scalper_bot.execution import adaptive_trailing
-from nifty_scalper_bot.execution import adaptive_trailing_legacy
+from nifty_scalper_bot.execution import adaptive_trailing, adaptive_trailing_core
 from nifty_scalper_bot.execution.hardened_adaptive_trailing import HardenedAdaptiveTrailingController
 
 
@@ -12,9 +11,8 @@ def test_adaptive_controller_identity_is_stable() -> None:
     controller = adaptive_trailing.AdaptiveTrailingController
     assert controller is HardenedAdaptiveTrailingController
     assert execution.AdaptiveTrailingController is controller
-    assert adaptive_trailing.LegacyAdaptiveTrailingController is adaptive_trailing_legacy.AdaptiveTrailingController
-    assert issubclass(controller, adaptive_trailing.LegacyAdaptiveTrailingController)
-    assert adaptive_trailing.TrailingSpec is adaptive_trailing_legacy.TrailingSpec
+    assert issubclass(controller, adaptive_trailing_core.AdaptiveTrailingController)
+    assert adaptive_trailing.TrailingSpec is adaptive_trailing_core.TrailingSpec
 
 
 def test_package_import_does_not_replace_adaptive_controller() -> None:
