86b7ec870db695196fe89bb3d7082b6261e29e7f
Kundakarlabp <kundakarlabp@gmail.com>
2026-07-06 15:01:06 +0530

    Install package candle overlap guard

--- src/nifty_scalper_bot/__init__.py
+++ src/nifty_scalper_bot/__init__.py
@@ -1,3 +1,10 @@
 """Nifty scalper bot package."""
 
+try:
+    from nifty_scalper_bot.data.pipeline_overlap_guard import install_candle_store_overlap_guard
+
+    install_candle_store_overlap_guard()
+except Exception:
+    pass
+
 __all__: list[str] = []
