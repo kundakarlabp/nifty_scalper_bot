a41a7e5d5d0145f4eeaa85e4ed0e9f7eb3cadbfa
Kundakarlabp <kundakarlabp@gmail.com>
2026-07-08 10:55:40 +0530

    Remove standalone flat alias test

--- tests/notifications/test_operator_flat_alias.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-
-def test_flat_alias_registered_for_operator_commands() -> None:
-    import nifty_scalper_bot.notifications  # noqa: F401 - triggers alias patch
-    from nifty_scalper_bot.notifications.operator_telegram import OPERATOR_COMMAND_NAMES
-
-    assert "flatten" in OPERATOR_COMMAND_NAMES
-    assert "flat" in OPERATOR_COMMAND_NAMES
