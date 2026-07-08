da3a1a3d1e76367dba5ef286b8b9ae0d9857a46e
Kundakarlabp <kundakarlabp@gmail.com>
2026-07-08 21:24:39 +0530

    Install OrderFlow live context proof patch

--- src/nifty_scalper_bot/strategies/elite_strategies/__init__.py
+++ src/nifty_scalper_bot/strategies/elite_strategies/__init__.py
@@ -9,10 +9,13 @@ from ..elite_tuesday_gamma_buyer import EliteTuesdayGammaBuyer
 from .oi_max_pain import OIMaxPainStrategy
 from .orb_pro import ORBProStrategy
 from .order_flow import OrderFlowStrategy
+from .order_flow_live_context_patch import apply_orderflow_live_context_patch
 from .rsi_divergence import RSIDivergenceStrategy
 from .smc_liquidity import SMCStrategy
 from .straddle_theta import StraddleThetaStrategy
 from .vwap_pro import VWAPProStrategy
+
+apply_orderflow_live_context_patch(OrderFlowStrategy)
 
 __all__ = [
     "BBSqueezeStrategy",
