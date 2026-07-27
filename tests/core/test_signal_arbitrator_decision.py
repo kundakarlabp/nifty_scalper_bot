cbaeb11624eeb7c7fc6aa76c0bd80fdae0b251bc
Kundakarlabp <kundakarlabp@gmail.com>
2026-07-27 10:31:01 +0530

    test: cover repeated option entry suppression

--- tests/core/test_signal_arbitrator_decision.py
+++ tests/core/test_signal_arbitrator_decision.py
@@ -1,3 +1,4 @@
+from nifty_scalper_bot.core import signal_arbitrator as arbitrator_module
 from nifty_scalper_bot.core.signal_arbitrator import SignalArbitrator, StrategyVote
 
 
@@ -28,3 +29,58 @@ def test_low_rr_hold():
     c = [{'symbol': 'NFO:NIFTYCE', 'side': 'CE', 'rr': 1.2, 'entry_price': 100, 'stop_loss': 95, 'target': 106, 'final_score': 9}]
     d = SignalArbitrator().decide(underlying='NIFTY', votes=votes, option_candidates=c, market_context={}, trace_id='t')
     assert d.action == 'HOLD' and 'rr_low' in d.reasons
+
+
+def test_release_starts_reentry_cooldown_from_exit_time(monkeypatch):
+    now = [1000.0]
+    monkeypatch.setattr(arbitrator_module.time, 'time', lambda: now[0])
+    arb = SignalArbitrator(
+        cooldown_seconds=3.0,
+        stale_active_seconds=120.0,
+        reentry_cooldown_seconds=300.0,
+    )
+    ce = 'NFO:NIFTY26JUL23950CE'
+
+    assert arb.allow(ce, 'BUY') is True
+    arb.register(ce, 'BUY')
+    now[0] += 180.0
+    arb.release(ce)
+
+    now[0] += 119.0
+    assert arb.allow(ce, 'BUY') is False
+    now[0] += 181.0
+    assert arb.allow(ce, 'BUY') is True
+
+
+def test_nifty_ce_and_pe_share_one_entry_reservation(monkeypatch):
+    now = [2000.0]
+    monkeypatch.setattr(arbitrator_module.time, 'time', lambda: now[0])
+    arb = SignalArbitrator(reentry_cooldown_seconds=300.0)
+    ce = 'NFO:NIFTY26JUL23950CE'
+    pe = 'NFO:NIFTY26JUL23950PE'
+
+    assert arb.allow(ce, 'BUY') is True
+    arb.register(ce, 'BUY')
+    assert arb.allow(pe, 'BUY') is False
+
+    arb.release(ce)
+    now[0] += 120.0
+    assert arb.allow(pe, 'BUY') is False
+    now[0] += 180.0
+    assert arb.allow(pe, 'BUY') is True
+
+
+def test_stale_active_reservation_fails_closed_for_reentry_window(monkeypatch):
+    now = [3000.0]
+    monkeypatch.setattr(arbitrator_module.time, 'time', lambda: now[0])
+    arb = SignalArbitrator(
+        stale_active_seconds=120.0,
+        reentry_cooldown_seconds=300.0,
+    )
+    symbol = 'NFO:NIFTY26JUL23950CE'
+
+    arb.register(symbol, 'BUY')
+    now[0] += 121.0
+    assert arb.allow(symbol, 'BUY') is False
+    now[0] += 300.0
+    assert arb.allow(symbol, 'BUY') is True
