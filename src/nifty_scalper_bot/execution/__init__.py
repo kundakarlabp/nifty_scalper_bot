04d3dcaea8bb194933638b99a0f9f1cfad4b0126
Kundakarlabp <kundakarlabp@gmail.com>
2026-09-11 18:01:35 +0530

    fix(execution): install dedicated broker P&L authority

--- src/nifty_scalper_bot/execution/__init__.py
+++ src/nifty_scalper_bot/execution/__init__.py
@@ -21,6 +21,7 @@ from nifty_scalper_bot.execution.operator_control_patch import apply_patches as 
 from nifty_scalper_bot.execution.protective_order_intent_patch import apply_patches as _apply_protective_order_intent_patches
 from nifty_scalper_bot.execution.position_risk_state_patch import apply_patches as _apply_position_risk_state_patches
 from nifty_scalper_bot.execution.stop_rearm_contract_patch import apply_patches as _apply_stop_rearm_contract_patches
+from nifty_scalper_bot.execution.broker_pnl_authority_patch import apply_patches as _apply_broker_pnl_authority_patches
 from nifty_scalper_bot.execution.pnl_nonblocking_patch import apply_patches as _apply_pnl_nonblocking_patches
 import nifty_scalper_bot.data.quote_identity_extension as _quote_identity_extension
 import nifty_scalper_bot.execution.bracket_ownership_extension as _bracket_ownership_extension
@@ -39,6 +40,7 @@ _broker_exposure_quarantine_extension.apply_patches()
 _broker_order_ledger_patch.apply_patches()
 _bracket_ownership_extension.apply_patches()
 _trade_plan_identity_guard.apply_patches()
+_apply_broker_pnl_authority_patches()
 _apply_pnl_nonblocking_patches()
 
 CanonicalBracketManager = BracketManager
